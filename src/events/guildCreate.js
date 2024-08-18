da8c91fb1ade8740e8fafdc6b47f05dd8ea48a84
ivo <ivolong@gmail.com>
2024-08-18 01:15:26 -0400

    Add links to guildCreate message

--- src/events/guildCreate.js
+++ src/events/guildCreate.js
@@ -10,6 +10,7 @@ module.exports = (client, guild) => {
   guild.channels.create('news-feed', { reason: 'HLTV news article updates channel by HLTV News bot.' })
     .then((channel) => {
       channel.send({
+        content: 'https://discord.gg/dE3NFqTzEx https://www.patreon.com/hltvnewsbot',
         embeds: [{
           title: 'Information',
           description: "Thanks for adding me.\n\nI'll post new articles from HLTV in here.\n\nDo you want notifications?\nType `/notify` and I'll give you a pingable role **if it exists**.\nType `/mute` to remove the role.\n\nType `/help` for general help.\n\nType `/invite` to invite me to your server.\n\nJoin [here](https://discord.gg/dE3NFqTzEx) for assistance.\n\nPlease consider supporting me on [Patreon](https://www.patreon.com/hltvnewsbot). Thank you!"
