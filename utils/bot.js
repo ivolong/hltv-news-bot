442d5d0e4b1b0619b2f9d811d33c29c1d78824d1
ivo <ivolong@gmail.com>
2024-08-17 21:38:00 -0400

    Refactor some main functions into utils

--- utils/bot.js
+++ utils/bot.js
@@ -1,3 +1,13 @@
+const fs = require('fs')
+const RssParser = require('rss-parser')
+const { REST } = require('@discordjs/rest')
+const { Routes } = require('discord-api-types/v9')
+const { Collection } = require('discord.js')
+
+const rss = new RssParser({
+  timeout: 5000
+})
+
 module.exports = {
   updateActivity: function (client) {
     const serverCount = client.guilds.cache.size
@@ -30,5 +40,59 @@ module.exports = {
         channel.send(embed).catch(() => {})
       }
     })
+  },
+
+  setCommands: function (client) {
+    const commands = []
+    const commandFiles = fs.readdirSync('../commands').filter(file => file.endsWith('.js'))
+
+    client.commands = new Collection()
+
+    for (const file of commandFiles) {
+      const command = require(`../commands/${file}`)
+      commands.push(command.data.toJSON())
+      client.commands.set(command.data.name, command)
+    }
+
+    if (process.env.DECLARE_SLASH_COMMANDS === 1) this.declareSlashCommands(commands)
+  },
+
+  declareSlashCommands: function (commands) {
+    console.log(`Declaring slash commands=${commands.size()}`)
+
+    const rest = new REST({ version: '9' }).setToken(process.env.DISCORD_REST_CLIENT_TOKEN);
+
+    (async () => {
+      try {
+        await rest.put(
+          Routes.applicationCommands(process.env.DISCORD_CLIENT_ID),
+          { body: commands }
+        )
+      } catch (error) {
+        console.error(error)
+      }
+    })()
+  },
+
+  rssChecker: function (name, url, client) {
+    const articleStorageFileLocation = `../storage/current_${name}_article.json`;
+
+    (async () => {
+      await rss.parseURL(url, function (error, feed) {
+        if (error) return console.error(error)
+
+        const newestArticle = feed.items[0]
+
+        const file = fs.readFileSync(articleStorageFileLocation)
+        const currentArticle = JSON.parse(file)
+
+        if (currentArticle.guid && newestArticle.guid !== currentArticle.guid) {
+          client.emit('newArticle', newestArticle)
+        }
+
+        const data = JSON.stringify(newestArticle)
+        fs.writeFileSync(articleStorageFileLocation, data)
+      })
+    })()
   }
 }
