dbd5291287d8767958a7adfeb18c64e67d7157cc
ivo <ivolong@gmail.com>
2024-08-18 01:12:07 -0400

    Slash command formatting changes

--- src/commands/help.js
+++ src/commands/help.js
@@ -6,6 +6,6 @@ module.exports = {
     .setDescription('Get help on using this bot'),
 
   async execute (interaction) {
-    interaction.reply("In order to work, this bot needs a channel called `#news-feed` to post articles in and (optionally) a role called `@hltv` to ping with notifications.\n\nWant notifications when there's a new article?\nType `/notify` and I'll give you a pingable role.\nType `/mute` to remove the role.\n\n`/invite` to invite me to your server.\n\nJoin [here](https://discord.gg/dE3NFqTzEx) for assistance.")
+    interaction.reply("In order to work, this bot needs a channel called `#news-feed` to post articles in and (optionally) a role called `@hltv` to ping with notifications.\n\nWant notifications when there's a new article?\nType `/notify` and I'll give you a pingable role.\nType `/mute` to remove the role.\n\n`/invite` to invite me to your server.\n\nJoin our server for further assistance: https://discord.gg/dE3NFqTzEx")
   }
 }
--- src/commands/invite.js
+++ src/commands/invite.js
@@ -6,6 +6,6 @@ module.exports = {
     .setDescription('Get HLTV News notifications in your server'),
 
   async execute (interaction) {
-    interaction.reply(`Click on my name (<@${process.env.DISCORD_CLIENT_ID}>), then add to server. Check out \` / help\` for assistance.`)
+    interaction.reply(`Click on my name (<@${process.env.DISCORD_CLIENT_ID}>), then add to server. Check out \`/help\` for assistance.`)
   }
 }
