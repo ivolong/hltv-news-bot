b13610f33846f1c071cae078faf46aad87e24db8
ivo <36451843+ivolong@users.noreply.github.com>
2024-09-02 17:59:18 -0400

    Use activity time as up time

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -5,9 +5,9 @@ const botUtils = require('../utils/bot.js')
 module.exports = (client: Client) => {
   console.log('Online')
 
+  botUtils.updateActivity(client)
+
   botUtils.setCommands(client)
 
-  setInterval(botUtils.rssChecker, 3e3, 'hltv', 'https://www.hltv.org/rss/news', client)
-
-  setInterval(botUtils.updateActivity, 60e3, client)
+  setInterval(botUtils.rssChecker, 5e3, 'hltv', 'https://www.hltv.org/rss/news', client)
 }
