465884103524a5f0b11d87ce73b6869b06de05e8
ivo <36451843+ivolong@users.noreply.github.com>
2024-09-02 22:11:37 -0400

    Nvm Discord clears activity after a while

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -5,9 +5,9 @@ const botUtils = require('../utils/bot.js')
 module.exports = (client: Client) => {
   console.log('Online')
 
-  botUtils.updateActivity(client)
-
   botUtils.setCommands(client)
+
+  setInterval(botUtils.updateActivity, 15 * 60e3, client)
 
   setInterval(botUtils.rssChecker, 5e3, 'hltv', 'https://www.hltv.org/rss/news', client)
 }
