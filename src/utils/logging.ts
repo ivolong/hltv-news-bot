23231c0ef9d706964e4bc53d8b05bbea9a977d97
ivo <ivolong@gmail.com>
2024-09-04 17:57:44 -0400

    Logging needs deterministic:false

--- src/utils/logging.ts
+++ src/utils/logging.ts
@@ -11,7 +11,7 @@ export const logger = winston.createLogger({
       const { timestamp, level, message, ...rest } = info
       return { timestamp, level, message, ...rest }
     })(),
-    winston.format.json(),
+    winston.format.json({ deterministic: false }),
     winston.format.colorize({ all: true }),
   ),
   transports: [
