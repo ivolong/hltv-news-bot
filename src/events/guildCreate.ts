04cd83d57fa48f8f70d37d844a5f001a5ffc4db4
ivo <ivolong@gmail.com>
2025-06-03 21:19:53 -0400

    feat: move the heart emoji

--- src/events/guildCreate.ts
+++ src/events/guildCreate.ts
@@ -34,7 +34,7 @@ module.exports = async (client: Client, guild: Guild) => {
               title: client.user?.displayName,
               color: 0x3c6ea1,
               url: "https://ko-fi.com/ivolong",
-              description: `I'll post new articles from HLTV in here. **Please do not rename this channel** but feel free to move it around.\n\nDo you want notifications?\nType ${notify} and I'll ping you with new stories.\nType ${mute} to stop being pinged.\n\nType ${help} for general help.\n\nType ${invite} to invite me to your server.\n\n**Need more help?** [Get help](https://discord.gg/dE3NFqTzEx)\n\n❤️ Please consider [supporting me](https://ko-fi.com/ivolong) running this bot - thank you!`,
+              description: `I'll post new articles from HLTV in here. **Please do not rename this channel** but feel free to move it around.\n\nDo you want notifications?\nType ${notify} and I'll ping you with new stories.\nType ${mute} to stop being pinged.\n\nType ${help} for general help.\n\nType ${invite} to invite me to your server.\n\n**Need more help?** [Get help](https://discord.gg/dE3NFqTzEx)\n\nPlease consider [supporting me](https://ko-fi.com/ivolong) running this bot - thank you! ❤️`,
               author: {
                 name: `Thank you for using ${client.user?.displayName}`,
                 icon_url: client.user?.defaultAvatarURL,
