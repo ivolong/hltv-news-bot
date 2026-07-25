5b6782b80145451e58eaa936d97faf002c197c76
ivo <36451843+ivolong@users.noreply.github.com>
2026-07-24 23:46:57 -0400

    feat(buttons): add more buttons (#49)

--- src/commands/invite.ts
+++ src/commands/invite.ts
@@ -18,8 +18,21 @@ export default {
       await interaction.client.application?.commands.fetch(),
     );
 
-    interaction.reply(
-      `Click on my name (<@${process.env.DISCORD_CLIENT_ID}>), then \`+ Add App\` and follow the required steps. Check out ${help} for assistance.`,
-    );
+    interaction.reply({
+      content: `Add me to your server and follow the required steps. Check out ${help} for assistance.`,
+      components: [
+        {
+          type: 1,
+          components: [
+            {
+              type: 2,
+              style: "LINK",
+              label: "Add to your server",
+              url: `https://discord.com/oauth2/authorize?client_id=${interaction.client.application?.id}`,
+            },
+          ],
+        },
+      ],
+    });
   },
 };
--- src/events/guildCreate.ts
+++ src/events/guildCreate.ts
@@ -18,8 +18,8 @@ export default async function guildCreate(client: Client, guild: Guild) {
     logger.warn("Unable to create role:", error);
   }
 
-  const [notify, mute, help, invite] = getSlashCommandString(
-    ["notify", "mute", "help", "invite"],
+  const [notify, mute, help] = getSlashCommandString(
+    ["notify", "mute", "help"],
     await client.application?.commands.fetch(),
   );
 
@@ -41,7 +41,7 @@ export default async function guildCreate(client: Client, guild: Guild) {
           title: client.user?.displayName,
           color: 0x3c6ea1,
           url: "https://ko-fi.com/ivolong",
-          description: `I'll post new articles from HLTV in here. **Please do not rename this channel** but feel free to move it around.\n\nDo you want notifications?\nType ${notify} and I'll ping you with new stories.\nType ${mute} to stop being pinged.\n\nType ${help} for general help.\n\nType ${invite} to invite me to your server.\n\n**Need more help?** [Get help](https://discord.gg/dE3NFqTzEx)\n\nPlease consider [supporting me](https://ko-fi.com/ivolong) running this bot - thank you! ❤️`,
+          description: `I'll post new articles from HLTV in here. **Please do not rename this channel** but feel free to move it around.\n\nDo you want notifications?\nType ${notify} and I'll ping you with new stories.\nType ${mute} to stop being pinged.\n\nType ${help} for general help.`,
           author: {
             name: `Thank you for using ${client.user?.displayName}`,
             icon_url: client.user?.displayAvatarURL(),
@@ -51,6 +51,31 @@ export default async function guildCreate(client: Client, guild: Guild) {
             text: "Not affiliated with HLTV.org or Better Collective A/S.",
             icon_url: client.user?.displayAvatarURL(),
           },
+        },
+      ],
+      components: [
+        {
+          type: 1,
+          components: [
+            {
+              type: 2,
+              style: "LINK",
+              label: "❤️ Support me",
+              url: "https://ko-fi.com/ivolong",
+            },
+            {
+              type: 2,
+              style: "LINK",
+              label: "Add to another server",
+              url: `https://discord.com/oauth2/authorize?client_id=${client.application?.id}`,
+            },
+            {
+              type: 2,
+              style: "LINK",
+              label: "Need more help?",
+              url: "https://discord.gg/dE3NFqTzEx",
+            },
+          ],
         },
       ],
     })
