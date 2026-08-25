977948ebb85bfede70bcf9f682747e0deb0694cd
ivo <36451843+ivolong@users.noreply.github.com>
2026-08-24 21:19:37 -0400

    feat(channels): send warning if a channel is renamed away (#63)

---
+++ src/events/channelUpdate.ts
@@ -0,0 +1,32 @@
+import { DMChannel, GuildChannel } from "discord.js";
+
+import { CHANNEL_NAME } from "../utils/bot";
+import { logger } from "../utils/logging";
+
+export default async function channelUpdate(
+  oldChannel: DMChannel | GuildChannel,
+  newChannel: DMChannel | GuildChannel,
+) {
+  if (
+    oldChannel instanceof DMChannel ||
+    newChannel instanceof DMChannel ||
+    !newChannel.isSendable()
+  )
+    return;
+
+  if (oldChannel.name == CHANNEL_NAME) {
+    logger.info("Supported channel was renamed");
+
+    newChannel.send(
+      `I see the \`#${CHANNEL_NAME}\` channel has been renamed. Please change the name back to \`#${CHANNEL_NAME}\` if you wish to continue receiving HLTV articles. I can only send HLTV articles to a channel named \`#${CHANNEL_NAME}\`, sorry.`,
+    );
+  }
+
+  if (newChannel.name == CHANNEL_NAME) {
+    logger.info("Unknown channel was renamed");
+
+    newChannel.send(
+      `I see this channel has been renamed \`#${CHANNEL_NAME}\`. I will post HLTV articles in here.`,
+    );
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
-import { Client, GatewayIntentBits } from "discord.js";
+import { Client, Events, GatewayIntentBits } from "discord.js";
 
+import channelUpdate from "./events/channelUpdate";
 import clientReady from "./events/clientReady";
 import guildCreate from "./events/guildCreate";
 import guildDelete from "./events/guildDelete";
@@ -10,10 +11,23 @@ const client = new Client({
   intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages],
 });
 
-client.on("clientReady", clientReady.bind(null, client));
-client.on("guildCreate", guildCreate.bind(null, client));
-client.on("guildDelete", guildDelete.bind(null, client));
-client.on("interactionCreate", interactionCreate.bind(null, client));
-client.on("newArticle", newArticle.bind(null, client));
+client.on(Events.ChannelUpdate, (oldChannel, newChannel) => {
+  channelUpdate(oldChannel, newChannel);
+});
+client.on(Events.ClientReady, (client) => {
+  clientReady(client);
+});
+client.on(Events.GuildCreate, (guild) => {
+  guildCreate(client, guild);
+});
+client.on(Events.GuildDelete, (guild) => {
+  guildDelete(client, guild);
+});
+client.on(Events.InteractionCreate, (interaction) => {
+  interactionCreate(client, interaction);
+});
+client.on("newArticle", (article) => {
+  newArticle(client, article);
+});
 
 client.login(process.env.DISCORD_CLIENT_TOKEN);
