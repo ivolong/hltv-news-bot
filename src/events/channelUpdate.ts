d1458ac8085164890f7d34c566852080a7d6ed2a
ivolong <ivolong@gmail.com>
2026-08-25 07:38:56 -0400

    fix(channel): don't alert unless name was changed

--- src/events/channelUpdate.ts
+++ src/events/channelUpdate.ts
@@ -14,7 +14,7 @@ export default async function channelUpdate(
   )
     return;
 
-  if (oldChannel.name == CHANNEL_NAME) {
+  if (oldChannel.name == CHANNEL_NAME && newChannel.name !== CHANNEL_NAME) {
     logger.info("Supported channel was renamed");
 
     newChannel.send(
@@ -22,7 +22,7 @@ export default async function channelUpdate(
     );
   }
 
-  if (newChannel.name == CHANNEL_NAME) {
+  if (newChannel.name == CHANNEL_NAME && oldChannel.name !== CHANNEL_NAME) {
     logger.info("Unknown channel was renamed");
 
     newChannel.send(
