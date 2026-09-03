272bf0c0b2aeba90dd42565591226f1de8ae1590
ivo <36451843+ivolong@users.noreply.github.com>
2026-09-02 20:28:00 -0400

    refactor: reduce reused code in stats posting (#77)

--- src/utils/stats.ts
+++ src/utils/stats.ts
@@ -1,84 +1,67 @@
 import { logger } from "./logging";
 
+const statBrokers = [
+  {
+    getUrl: (id: string) => `https://top.gg/api/bots/${id}/stats`,
+    getToken: () => process.env.TOPGG_CLIENT_TOKEN,
+    getAuthHeader: (token: string) => `${token}`,
+    getBody: (guildCount: number) => {
+      return { server_count: guildCount };
+    },
+  },
+  {
+    getUrl: (id: string) => `https://discord.bots.gg/api/v1/bots/${id}/stats`,
+    getToken: () => process.env.DISCORD_BOTS_GG_TOKEN,
+    getAuthHeader: (token: string) => `${token}`,
+    getBody: (guildCount: number) => {
+      return { guildCount };
+    },
+  },
+  {
+    getUrl: (id: string) => `https://api.botlist.me/api/v1/bots/${id}/stats`,
+    getToken: () => process.env.BOTLIST_ME_CLIENT_TOKEN,
+    getAuthHeader: (token: string) => `${token}`,
+    getBody: (guildCount: number) => {
+      return { server_count: guildCount };
+    },
+  },
+  {
+    getUrl: (id: string) => `https://api.discordlist.gg/v0/bots/${id}/guilds`,
+    getToken: () => process.env.DISCORD_LIST_CLIENT_TOKEN,
+    getAuthHeader: (token: string) => `Bearer ${token}`,
+    getBody: (guildCount: number) => {
+      return { count: guildCount };
+    },
+  },
+];
+
 export async function updateStats(guildCount: number) {
-  updateTopGg(guildCount);
-  updateDiscordBotsGg(guildCount);
-  updateBotlistMe(guildCount);
-  updateDiscordList(guildCount);
-}
+  statBrokers.forEach(async (broker) => {
+    const token = broker.getToken();
+    if (!token) return;
 
-async function update(url: string, request: RequestInit) {
-  let response;
-  try {
-    response = await fetch(url, request);
-  } catch (error) {
-    logger.warn("Error while posting statistics", url, error);
-    return;
-  }
+    const request = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: broker.getAuthHeader(token),
+      },
+      body: JSON.stringify(broker.getBody(guildCount)),
+    };
 
-  if (!response.ok) {
-    logger.warn("Unexpected response while posting statistics", url, {
-      response: await response.text(),
-    });
-  }
-}
+    const url = broker.getUrl(process.env.DISCORD_CLIENT_ID!);
+    let response;
+    try {
+      response = await fetch(url, request);
+    } catch (error) {
+      logger.warn("Error while posting statistics", url, error);
+      return;
+    }
 
-async function updateTopGg(guildCount: number) {
-  if (!process.env.TOPGG_CLIENT_TOKEN) return;
-
-  const url = `https://top.gg/api/bots/${process.env.DISCORD_CLIENT_ID!}/stats`;
-
-  await update(url, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: process.env.TOPGG_CLIENT_TOKEN,
-    },
-    body: JSON.stringify({ server_count: guildCount }),
-  });
-}
-
-async function updateDiscordBotsGg(guildCount: number) {
-  if (!process.env.DISCORD_BOTS_GG_TOKEN) return;
-
-  const url = `https://discord.bots.gg/api/v1/bots/${process.env.DISCORD_CLIENT_ID!}/stats`;
-
-  await update(url, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: process.env.DISCORD_BOTS_GG_TOKEN,
-    },
-    body: JSON.stringify({ guildCount }),
-  });
-}
-
-async function updateBotlistMe(guildCount: number) {
-  if (!process.env.BOTLIST_ME_CLIENT_TOKEN) return;
-
-  const url = `https://api.botlist.me/api/v1/bots/${process.env.DISCORD_CLIENT_ID!}/stats`;
-
-  await update(url, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: process.env.BOTLIST_ME_CLIENT_TOKEN,
-    },
-    body: JSON.stringify({ server_count: guildCount }),
-  });
-}
-
-async function updateDiscordList(guildCount: number) {
-  if (!process.env.DISCORD_LIST_CLIENT_TOKEN) return;
-
-  const url = `https://api.discordlist.gg/v0/bots/${process.env.DISCORD_CLIENT_ID!}/guilds`;
-
-  await update(url, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: `Bearer ${process.env.DISCORD_LIST_CLIENT_TOKEN}`,
-    },
-    body: JSON.stringify({ count: guildCount }),
+    if (!response.ok) {
+      logger.warn("Unexpected response while posting statistics", url, {
+        response: await response.text(),
+      });
+    }
   });
 }
